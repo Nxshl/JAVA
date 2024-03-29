236c9d0a80fdbe26916f5999e9fe504f3f158c7e
Nishal <142426014+Nxshl@users.noreply.github.com>
2024-03-29 19:54:24 +0530

    Add files via upload

---
+++ java/rectangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class rectangle{
+
+    double length;
+    double breadth;
+
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the breadth of the rectangle: ");
+        double breadth = scanner.nextDouble();
+
+        double area = length * breadth;
+        double perimeter = 2 * (length + breadth);
+
+        System.out.println("The area of the rectangle is : " + area);
+        System.out.println("The perimeter of the retangle is: " + perimeter);
+
+        scanner.close();
+    }
+}
